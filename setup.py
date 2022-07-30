214c36cbf03d77b5c7a8fb8b1ac1b21f844578cc
Malte Wächter <git@malte-waechter.com>
2022-07-31 01:05:43 +0200

    Setup package structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+FaaS-Profiler Core
+"""
+
+import setuptools
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setuptools.setup(
+    name='faas-profiler-core',
+    version='0.0.1',
+    url='https://github.com/spcl/faas-profiler-core',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=requirements
+)
