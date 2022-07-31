18cb490ee147d9b2969d65c5cdfc37c7006691b1
Malte Wächter <git@malte-waechter.com>
2022-07-31 10:53:23 +0200

    Add logging module

---
+++ faas_profiler_core/logging.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Logging
+"""
+
+import logging
+
+class Loggable:
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
